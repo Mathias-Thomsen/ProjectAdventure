dbd22fff195dc8ba73a1447486070b46b40de746
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-28 19:50:37 +0200

    reformat code

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -6,30 +6,25 @@ public class UserInterface {
         Scanner scanner = new Scanner(System.in);
         Adventure adventure = new Adventure();
         String userInput;
+        System.out.println("You are in " + adventure.getSelectedRoom().getRoomName() + " Where do you want to go");
 
         do {
 
-            System.out.println("You are in " + adventure.getSelectedRoom().getRoomName() + " Where do you want to go");
             userInput = scanner.nextLine();
 
-            if (userInput.equals("l")){
+            if (userInput.equals("l")) {
                 System.out.println(adventure.getSelectedRoom().getRoomInfo());
-            }
-            else if (adventure.go(userInput)){
-                System.out.println(adventure.getSelectedRoom().getRoomName());
+            } else if (adventure.go(userInput)) {
+                System.out.println("You are now in: " + adventure.getSelectedRoom().getRoomName());
             } else {
                 System.out.println("You can not go that way");
             }
 
 
-
-
-
-
-        }while (userInput != "exit");
+        } while (userInput != "exit");
     }
 
-    public void start(){
+    public void start() {
         user();
     }
 }
